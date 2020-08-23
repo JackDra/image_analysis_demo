5e8f57a70924df24067e125d04561be80003247a
jdragos <jdragos@enthought.com>
2020-08-23 12:27:51 -0500

    from disk

--- image_analysis.py
+++ image_analysis.py
@@ -1,17 +1,25 @@
-from traits.api import HasStrictTraits, Instance, DelegatesTo, on_trait_change
-from traitsui.api import UItem, View
+from traits.api import (
+    HasStrictTraits, Instance, DelegatesTo, on_trait_change, File
+)
+from traitsui.api import UItem, View, Item
 from enable.component_editor import ComponentEditor
 
 from chaco.api import Plot, ImageData, ImagePlot
 import numpy as np
+from PIL import Image
+import os
 
 # generate a test image to display
-BLANK_IMG = np.arange(800*600*3)
-BLANK_IMG = BLANK_IMG.reshape(800, 3, 600)
-BLANK_IMG = BLANK_IMG.swapaxes(1,2)
-BLANK_IMG = BLANK_IMG.astype(np.uint8) % 252
+BLANK_IMG = np.zeros((1920, 1080, 3)).astype(np.uint8)
+
+def load_image(image_path):
+    """ load an image from disk """
+    pil_image = Image.open(image_path)
+    return np.array(pil_image).swapaxes(0, 1)[::-1, :, :]
 
 class ImageAnalysis(HasStrictTraits):
+
+    image_file = File(filter=['*.jpg','*.png'])
 
     plot = Instance(Plot)
 
@@ -20,6 +28,10 @@ class ImageAnalysis(HasStrictTraits):
     img_plot = Instance(ImagePlot)
 
     data = DelegatesTo('plot_data')
+
+    def _image_file_changed(self):
+        if os.path.isfile(self.image_file):
+            self.data = load_image(self.image_file)
 
     def _plot_data_default(self):
         return ImageData(data=BLANK_IMG)
@@ -33,6 +45,7 @@ class ImageAnalysis(HasStrictTraits):
 
     def default_traits_view(self):
         return View(
+            Item('image_file'),
             UItem('plot', editor=ComponentEditor()),
             resizable=True
         )
